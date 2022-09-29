b2a64f95f5019261fdbe39c4f50f3222c8e5e2eb
pan <zixiang929@163.com>
2022-09-29 10:43:24 +0800

    hot-fix2 commit

--- src/main/java/com/yutuai/GitTest.java
+++ src/main/java/com/yutuai/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello pan");
         System.out.println("hello pzx");
+        System.out.println("hello pan4");
     }
 }
