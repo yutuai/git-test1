bd47cb955700d4190f399dadf6b4ef15dff7d09b
pan <zixiang929@163.com>
2022-09-29 10:55:59 +0800

    master2 commit

--- src/main/java/com/yutuai/GitTest.java
+++ src/main/java/com/yutuai/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello master");
         System.out.println("hello hot-fix");
+        System.out.println("push test");
     }
 }
